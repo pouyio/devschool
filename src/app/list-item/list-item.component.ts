5fbbc91f6ec9ad2520ffd6b5495dfb162f04fb7b
Vicente <pouyio@gmail.com>
2018-09-02 16:56:39 +0200

    created items service

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Item } from '../models';
 import { Output } from '@angular/core';
 import { Input } from '@angular/core';
+import { ItemsService } from '../services/items.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'dev-list-item',
@@ -10,61 +12,15 @@ import { Input } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
-  items: Item[];
+  items$: Observable<Item[]>;
   filterAvailable: boolean;
 
   @Input() language: string;
   @Output() selectedItem: EventEmitter<Item>;
 
-  constructor() {
+  constructor(private itemsService: ItemsService) {
     this.selectedItem = new EventEmitter();
-    this.items = [
-      {
-        id: 1,
-        name: 'armadura',
-        description: 'Es una armadura estupenda',
-        price: 20,
-        currency: 'EUR',
-        available: true,
-        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
-      },
-      {
-        id: 2,
-        name: 'escudo',
-        description: 'Es un escudo magnífico',
-        price: 29.6,
-        currency: 'EUR',
-        available: false,
-        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
-      },
-      {
-        id: 3,
-        name: 'casco',
-        description: 'Es un casco genial',
-        price: 15.9991,
-        currency: 'USD',
-        available: false,
-        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
-      },
-      {
-        id: 4,
-        name: 'pala',
-        description: 'Es una pala estratosférica',
-        price: 9.99,
-        currency: 'USD',
-        available: true,
-        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
-      },
-      {
-        id: 5,
-        name: 'caballo',
-        description: 'Es un caballo muy cariñoso',
-        price: 980,
-        currency: 'USD',
-        available: true,
-        img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
-      }
-    ];
+    this.items$ = this.itemsService.getItems();
    }
 
   ngOnInit() {
---
+++ src/app/services/items.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Item } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  private items: Item[];
+
+  constructor() {
+    this.items = [{
+      id: 1,
+      name: 'armadura',
+      description: 'Es una armadura estupenda',
+      price: 20,
+      currency: 'EUR',
+      available: true,
+      img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
+    },
+    {
+      id: 2,
+      name: 'escudo',
+      description: 'Es un escudo magnífico',
+      price: 29.6,
+      currency: 'EUR',
+      available: false,
+      img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
+    },
+    {
+      id: 3,
+      name: 'casco',
+      description: 'Es un casco genial',
+      price: 15.9991,
+      currency: 'USD',
+      available: false,
+      img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
+    },
+    {
+      id: 4,
+      name: 'pala',
+      description: 'Es una pala estratosférica',
+      price: 9.99,
+      currency: 'USD',
+      available: true,
+      img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
+    },
+    {
+      id: 5,
+      name: 'caballo',
+      description: 'Es un caballo muy cariñoso',
+      price: 980,
+      currency: 'USD',
+      available: true,
+      img: 'https://uploads-ssl.webflow.com/57e5747bd0ac813956df4e96/5aebae14c6d254621d81f826_placeholder.png'
+    }];
+  }
+
+  getItems(): Observable<Item[]> {
+    return of(this.items);
+  }
+}
