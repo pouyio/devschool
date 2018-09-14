18a82a440a638fbfe5ff9e7ac01b9ab17dfbeae2
Vicente Ortiz García-Cervigón <vortizga@everis.com>
2018-09-14 16:46:36 +0200

    removed tests

--- src/app/filter-available.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterAvailablePipe } from './filter-available.pipe';
-
-describe('FilterAvailablePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterAvailablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
